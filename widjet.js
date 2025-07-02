84e9dba101582bfb36f4e6975acb153337d2f8e0
Harini Kumaran <136872295+Harinikuma@users.noreply.github.com>
2025-07-02 10:14:33 +0530

    Update widjet.js

--- widjet.js
+++ widjet.js
@@ -3,10 +3,10 @@
   if (!container) return;
 
   container.innerHTML = `
-    <div style="display: flex; gap: 10px; align-items: center; margin-top: 20px;">
-      <input type="text" placeholder="Enter value 1" style="padding: 8px;" />
-      <input type="text" placeholder="Enter value 2" style="padding: 8px;" />
-      <button style="padding: 8px 16px; cursor: pointer;">Submit</button>
+    <div style="display: flex; gap: 10px; align-items: center; margin-top: 20px; padding: 30px; ">
+      <input type="text" placeholder="Enter value 1" style="margin: 10px;" />
+      <input type="text" placeholder="Enter value 2" style="margin: 10px;" />
+      <button style="margin:10px;padding: 8px 16px; cursor: pointer; background-Color: #07a3eb; border-radius: 10px">Submit</button>
     </div>
   `;
 })();
